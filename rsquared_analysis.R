901ce050d37af15627bccd1e8a23a5bb9274f5a6
Timo Riske <timo.riske@pff.com>
2022-08-21 11:12:58 +0200

    Update rsquared_analysis.R

--- rsquared_analysis.R
+++ rsquared_analysis.R
@@ -110,19 +110,24 @@ player_games <- player_games %>%
   tidyr::crossing(play_in_game = 1:MAX_PLAYS) %>%
   filter(play_in_game <= N)
 player_games <- player_games %>% group_by(player_season_id,sim_id) %>%
-  mutate(x = lag(x0) + rnorm(n(), mean = season_tendency, sd = 0.05),
+  mutate(trend = rnorm(n(), mean = season_tendency, sd = 0.05),
+         x = lag(x0) + trend,
          x = ifelse(is.na(x), x0, x),
          x_final = last(x)) %>% ungroup()
 player_games <- player_games %>% group_by(player_game_id,sim_id) %>%
   mutate(x0_game = x[1]) %>% ungroup()
 
 player_games <- player_games %>%
-  mutate(y = x + rnorm(nrow(player_games), mean = season_tendency, sd = sqrt(500)),
-         y2 = x + rnorm(nrow(player_games), mean = season_tendency, sd = sqrt(400)))
+  mutate(y = x + rnorm(nrow(player_games), mean = trend, sd = sqrt(500)),
+         y2 = x + rnorm(nrow(player_games), mean = trend, sd = sqrt(400)))
 player_games_agg <- player_games %>% group_by(player_game_id,player_season_id,sim_id,N,x0,x0_game) %>%
   summarise(y = mean(y), y2 = mean(y2)) %>% ungroup()
 player_seasons_agg <- player_games %>% group_by(player_season_id,sim_id,x0,x_final) %>%
   summarise(N = sum(N), y = mean(y), y2 = mean(y2)) %>% ungroup()
+
+player_seasons_agg %>%
+  ggplot(aes(x = x_final - x0)) +
+  geom_histogram(bins = 100)
 
 df_r2_seasons <- player_seasons_agg %>% group_by(sim_id) %>%
   summarise(
@@ -174,7 +179,7 @@ a <- df_r2_seasons %>% ggplot() + fte_theme() + theme(legend.position = "top") +
        title = "R-squared values come with uncertainty",
        subtitle = "Simulation of R^2 values in 1000 parallel universes of two metrics with different predictive power",
        caption = "Predicted seasons: 5 | Players per season: 100 | Plays per season: 500")
-ggsave(glue("../git_repo/public_stuff/viz/r_squared_distribution_season_true.png"),a,dpi = 600, width = 10, height = 6)
+ggsave(glue("../git_repo/public_stuff/viz/r_squared_distribution_season_dynamic.png"),a,dpi = 600, width = 10, height = 6)
 
 
 a <- df_r2_games %>% ggplot() + fte_theme() + theme(legend.position = "top") +
@@ -185,7 +190,7 @@ a <- df_r2_games %>% ggplot() + fte_theme() + theme(legend.position = "top") +
        title = "R-squared values come with less uncertainty on the game-level",
        subtitle = "Simulation of R^2 values in 1000 parallel universes of two metrics with different predictive power",
        caption = "Predicted games: 5*17 | Players per game: 100 | Plays per game: 500/17")
-ggsave(glue("../git_repo/public_stuff/viz/r_squared_distribution_game_true.png"),a,dpi = 600, width = 10, height = 6)
+ggsave(glue("../git_repo/public_stuff/viz/r_squared_distribution_game_dynamic.png"),a,dpi = 600, width = 10, height = 6)
 
 a <- df_r2_plays %>% ggplot() + fte_theme() + theme(legend.position = "top") +
   geom_density(aes(x = r2_500, fill = "500"), alpha = 0.6, color = "black") +
@@ -195,4 +200,4 @@ a <- df_r2_plays %>% ggplot() + fte_theme() + theme(legend.position = "top") +
        title = "R-squared values come with less uncertainty on the play-level",
        subtitle = "Simulation of R^2 values in 1000 parallel universes of two metrics with different predictive power",
        caption = "Predicted plays: 500*500")
-ggsave(glue("../git_repo/public_stuff/viz/r_squared_distribution_play_true.png"),a,dpi = 600, width = 10, height = 6)
+ggsave(glue("../git_repo/public_stuff/viz/r_squared_distribution_play_dynamic.png"),a,dpi = 600, width = 10, height = 6)
